292e6d23a86c87cb22503a3b165ed70af875a0f1
mkonnerth <matkonnerth@gmail.com>
2020-05-24 09:23:45 +0200

    add nodeset

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# nodesetLoader_usage
+# How to use nodesetLoader with open62541
+
+## Build and run
+
+git clone https://github.com/matkonnerth/nodesetLoader_usage.git
+cd nodesetLoader_usage
+mkdir build
+cd build
+cmake ..
+make
+./server
--- main.c
+++ main.c
@@ -1,9 +1,24 @@
 #include <NodesetLoader/backendOpen62541.h>
+#include <open62541/plugin/log_stdout.h>
 #include <open62541/server.h>
 #include <open62541/server_config_default.h>
+
+static volatile UA_Boolean running = true;
+static void stopHandler(int sign) {
+  UA_LOG_INFO(UA_Log_Stdout, UA_LOGCATEGORY_SERVER, "received ctrl-c");
+  running = false;
+}
 
 int main(int argc, const char *argv[]) {
   UA_Server *server = UA_Server_new();
   UA_ServerConfig_setDefault(UA_Server_getConfig(server));
-  NodesetLoader_loadFile(server, "unknownPath", NULL);
+  //provide the server and the path to nodeset
+  //returns true in case of successful import
+  if(!NodesetLoader_loadFile(server, "../Opc.Ua.Di.NodeSet2.xml", NULL))
+  {
+    UA_LOG_INFO(UA_Log_Stdout, UA_LOGCATEGORY_USERLAND, "importing the xml nodeset failed");
+  }
+  UA_StatusCode retval = UA_Server_run(server, &running);
+  UA_Server_delete(server);
+  return retval == UA_STATUSCODE_GOOD ? EXIT_SUCCESS : EXIT_FAILURE;
 }
